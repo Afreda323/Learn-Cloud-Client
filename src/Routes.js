b9d97c1ae2c8887cbb26047e1a0a0daf6e7ff500
Anthony Freda <anthonyfreda323@gmail.com>
2018-11-15 14:45:55 -0500

    codesplit routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,45 +1,49 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Switch, Route } from 'react-router-dom'
-import Home from './containers/Home'
-import Login from './containers/Login'
-import Signup from './containers/Signup'
-import NotFound from './containers/NotFound'
-import NewNote from './containers/NewNote'
-import Notes from './containers/Notes'
+
 import AuthRoute from './components/AuthRoute'
 import ProtectedRoute from './components/ProtectedRoute'
 import UnProtectedRoute from './components/UnProtectedRoute'
 
+const Home = lazy(() => import('./containers/Home'))
+const Login = lazy(() => import('./containers/Login'))
+const Signup = lazy(() => import('./containers/Signup'))
+const NotFound = lazy(() => import('./containers/NotFound'))
+const NewNote = lazy(() => import('./containers/NewNote'))
+const Notes = lazy(() => import('./containers/Notes'))
+
 export default ({ childProps }) => {
   return (
-    <Switch>
-      <AuthRoute exact path="/" component={Home} props={childProps} />
-      <UnProtectedRoute
-        path="/login"
-        exact
-        component={Login}
-        props={childProps}
-      />
-      <UnProtectedRoute
-        path="/signup"
-        exact
-        component={Signup}
-        props={childProps}
-      />
-      <ProtectedRoute
-        path="/newNote"
-        exact
-        component={NewNote}
-        props={childProps}
-      />
-      <ProtectedRoute
-        path="/notes/:id"
-        exact
-        component={Notes}
-        props={childProps}
-      />
+    <Suspense fallback={'Loading...'}>
+      <Switch>
+        <AuthRoute exact path="/" component={Home} props={childProps} />
+        <UnProtectedRoute
+          path="/login"
+          exact
+          component={Login}
+          props={childProps}
+        />
+        <UnProtectedRoute
+          path="/signup"
+          exact
+          component={Signup}
+          props={childProps}
+        />
+        <ProtectedRoute
+          path="/newNote"
+          exact
+          component={NewNote}
+          props={childProps}
+        />
+        <ProtectedRoute
+          path="/notes/:id"
+          exact
+          component={Notes}
+          props={childProps}
+        />
 
-      <Route component={NotFound} />
-    </Switch>
+        <Route component={NotFound} />
+      </Switch>
+    </Suspense>
   )
 }
